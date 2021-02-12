361398f118206ef9293b38421542790c746def25
Jeremy Miller <gkstaff@gmail.com>
2021-02-12 12:55:20 -0800

    add more edits to main scripts.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,17 +1,13 @@
-import $ from 'jquery';
-
-
-export class currency {
-  static currencyConverter() {
-    return fetch(`https://v6.exchangerate-api.com/v6/?api_key=${process.env.API_KEY}/latest/USD`)
-      .then(function (response) {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        return response.json();
-      })
-      .catch(function (error) {
-        return error;
-      });
+export default class Currency {
+  static async currencyConverter() {
+    try {
+      const response = fetch(`http://v6.exchangerate-api.com/v6/?api_key=${process.env.API_KEY}/latest/USD`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch (error) {
+      return error;
+    }
   }
 }
---
+++ src/main.js
@@ -0,0 +1,26 @@
+import $ from 'jquery';
+import Currency from './js/scripts.js';
+
+
+
+function getElements(response) {
+  if (response.ok) {
+    console.log("good juju");
+  } else {
+    console.log("bad juju");
+  }
+}
+
+
+
+$(document).ready(function () {
+  // event.preventDefault();
+
+
+  Currency.currencyConverter()
+    .then(function (response) {
+      getElements(response);
+    });
+
+
+});
